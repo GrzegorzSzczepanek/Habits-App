81d40a3dcfa0d4bcc1b50e48c2837a9da9087174
Grzesiek <grzegorzszczepanek48@gmail.com>
2023-02-21 16:22:02 -0500

    Add files via upload

---
+++ create.py
@@ -0,0 +1,112 @@
+import os
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+
+def add_objective():
+    global select_type_window
+    select_type_window = Toplevel()
+    select_type_window.resizable(False, False)
+
+    yes_no_objective_btn = Button(select_type_window,
+                                  text="YES/NO\nf.e. Did you wake up at 6AM",
+                                  bg="#222",
+                                  fg="#FFF",
+                                  font=("Arial", 21),
+                                  command=add_yes_no_objective
+                                  ).grid(row=0)
+
+    measurable_objective_btn = Button(select_type_window,
+                                  text="Measurable Objective\nf.e. How many book pages have you read today",
+                                  bg="#222",
+                                  fg="#FFF",
+                                  font=("Arial", 21),
+                                  command=add_measurable_objective
+                                  ).grid(row=1)
+
+
+def add_yes_no_objective():
+    _button_list = select_type_window.winfo_children()
+    _button_list[0].destroy()
+    _button_list[1].destroy()
+
+    name = Label(select_type_window,
+                  text="Name of task").grid(row=0)
+
+    question = Label(select_type_window,
+                      text="Question").grid(row=1)
+
+
+    period_variable1 = StringVar()
+    period_variable2 = StringVar()
+    period_variable3 = StringVar()
+
+    frequency = Label(select_type_window,
+                      text="Frequency").grid(row=2)
+
+    remainder = Label(select_type_window,
+                      text="Remainder").grid(row=3)
+
+    notes = Label(select_type_window,
+                      text="Notes").grid(row=4)
+
+    name_entry = Entry(select_type_window).grid(row=0, column=1)
+    question_entry = Entry(select_type_window).grid(row=1, column=1)
+    # frequency_frame = Frame(select_type_window).grid(row=2, column=1)
+    Radiobutton(select_type_window, text="Everyday", value="everyday", variable=period_variable1).pack()
+    Radiobutton(select_type_window, text="Weekly", value="everyday", variable=period_variable2).pack()
+    Radiobutton(select_type_window, text="Weekly", value="everyday", variable=period_variable3).pack()
+    
+    
+    remainder_entry = Entry(select_type_window).grid(row=3, column=1)
+    
+    notes_entry = Entry(select_type_window).grid(row=4, column=1)
+
+    Button(select_type_window, text="Back",command=get_back).grid(row=5, column=0)
+    Button(select_type_window, text="Add", command=create_obejctive("YN")).grid(row=5, column=1)
+
+
+def add_measurable_objective():
+    _button_list = select_type_window.winfo_children()
+    _button_list[0].destroy()
+    _button_list[1].destroy()
+
+
+def create_obejctive(objective_type):
+    pass
+
+
+def get_back():
+    pass
+
+def open_settings():
+    dark_theme = True
+
+    settings_window = Toplevel()
+    settings_window.title("Settings")
+    settings_window.geometry("250x500")
+    settings_label = Label(settings_window,
+                           text="Test Label").pack()
+
+    night_mode_btn = Button(settings_window,
+                            text="Night mode",
+                            command=change_theme(dark_theme)).pack()
+
+    language_btn = Button(settings_window,
+                          text="Change language").pack()
+
+
+def change_theme(current_theme):
+    pass
+
+
+def create_saves_folder():
+    if os.path.isdir("C:\\HbtsApp"):
+        return
+    else:
+        check_path = os.path.join("C:\\", "HbtsApp")
+        os.makedirs(check_path)
+        source_directory = filedialog.askdirectory(title="Selected Directory will be used t save your data and settings")
+        path = os.path.join(source_directory, "Saves")
+        os.makedirs(path)
+    return path
---
+++ main.py
@@ -0,0 +1,41 @@
+from create import *
+from tkinter import *
+
+window = Tk()
+
+width = 700
+height = 250
+window.geometry(str(width) + "x" + str(height))
+
+center_frame = Frame(window)
+center_frame.pack(side="right")
+# center_frame.grid(row=0, column=1)
+
+menu_frame = Frame(window,
+                   bg="#d45",
+                   height=height,
+                   width=int(width))
+menu_frame.pack(side="left", fill="y")
+
+add_objective_btn = Button(menu_frame,
+                           text="Add Objective",
+                           command=add_objective,
+                           padx=10,
+                           pady=10,
+                           )
+add_objective_btn.pack(pady=20, padx=20)
+
+settings_btn = Button(menu_frame,
+                           text="Settings",
+                           command=open_settings,
+                           padx=10,
+                           pady=10,
+                           width=10
+                           )
+settings_btn.pack(pady=20, padx=20)
+
+
+# One way to check wheather user picked saves folder once
+saves_path = create_saves_folder()
+
+window.mainloop()
